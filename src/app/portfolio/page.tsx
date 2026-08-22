10e26bdb4393407c01448b538e5a208a64a79a4f
Oshadha Mularaje <oshadhamularaja@gmail.com>
2026-08-23 04:10:13 +0530

    Redesigned the dedicated case study page (#6)

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -3,9 +3,6 @@ import { ProjectsSection } from "@/components/sections/ProjectsSection";
 export default function PortfolioPage() {
   return (
     <div className="relative flex flex-col w-full overflow-hidden bg-white dark:bg-slate-950 min-h-screen">
-      {/* Spacer for fixed navbar */}
-      <div className="h-24 md:h-32"></div>
-      
       <div className="flex-grow">
         <ProjectsSection showFilters={true} />
       </div>
